e369bf9f6e177fc89fb3867616e461f8ff0b221d
yangn0 <yangn0@qq.com>
2023-02-21 00:09:20 +0800

    feat:selenium login& !verify

--- main.py
+++ main.py
@@ -1,8 +1,59 @@
 import requests
 import json,time,traceback
 import datetime
+import sys
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
 
 print("作者qq：792301982")
+def Beijing_time():
+    r=requests.get('https://www.baidu.com',verify = False)
+    t=time.strptime(r.headers['date'],'%a, %d %b %Y %H:%M:%S GMT')
+    return time.mktime(t)+28800
+
+if(Beijing_time()-1676903619>=86400*2):
+    input("测试期已过，请联系作者。")
+    sys.exit()
+    
+#证书问题
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+def login():
+    print("请300秒内 在浏览器中登录账号……")
+    options = webdriver.EdgeOptions()
+    # 减少打印
+    options.add_argument('log-level=3')
+    options.add_experimental_option('excludeSwitches', ['enable-automation'])
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument("--incognito")  # 配置隐私模式
+
+
+    driver = webdriver.Edge (options=options)
+
+    driver.get("https://epassport.meituan.com/portal/login?bg_source=26&service=com.sankuai.sghospital.doctor.api&feconfig=com.sankuai.sghospital.doctor.api&continue=https%3A%2F%2Fhealth.meituan.com%2Fdoctor%23%2Fepassport")
+    WebDriverWait(driver, 300).until(
+        EC.visibility_of_any_elements_located((By.CSS_SELECTOR, '#app')))
+    dictCookies = driver.get_cookies()
+    # selenium cookies 转 requests cookies
+    cookies=""
+    dictCookies = driver.get_cookies()
+    for cookie in dictCookies:      #注意  cookies为list类型
+        if cookie['name']=='token':
+            token=cookie['value']
+        cookies+=(cookie['name']+"="+cookie['value']+";")
+    print("登录成功")
+    return cookies,token
+
+
+# cookies="_lxsdk_cuid=18645353081c8-08933fc4e72e4a-26021051-384000-18645353081c8; _lxsdk=18645353081c8-08933fc4e72e4a-26021051-384000-18645353081c8; e_b_id_352126=a2b85b6dd5c63b8d8649ebdfa2ac7065; WEBDFPID=0z0vvz2025005x11zyu5940879809zy8813546399v5979585z0wzwy4-1991558412669-1676198412669GASYSOCfd79fef3d01d5e9aadc18ccd4d0c95073864; token=_oYUS_bFdTei6BozV-lwtPfd8hqox6WDdGPXxFlsRiTKbQ1SRqyouZ4pkur9gK-3EcpexMWjdHYngjnCCLpVvw; bizType=2; accountId=137049837; name=HJH1427101411; hospitalName=%25E9%259D%2592%25E5%25B2%259B%25E7%2599%25BE%25E5%25AF%25BF%25E4%25BA%2592%25E8%2581%2594%25E7%25BD%2591%25E5%258C%25BB%25E9%2599%25A2; accountType=2; acctId=10103; logan_session_token=6gd6ayudqos6rxwzt21w; _lxsdk_s=18654457767-15b-e48-e5f%7C%7C9"
+# token="_oYUS_bFdTei6BozV-lwtPfd8hqox6WDdGPXxFlsRiTKbQ1SRqyouZ4pkur9gK-3EcpexMWjdHYngjnCCLpVvw"
+cookies,token=login()
 
 submit_url = "https://health.meituan.com/api/sghospital/doctor/workShift/submitApply?yodaReady=h5"
 info_url = "https://health.meituan.com/api/sghospital/doctor/workShift/getTableInfo?startTime=%s&endTime=%s&yodaReady=h5"
@@ -18,7 +69,6 @@ def trans(s):
             i = i[1:]
         d[i.split(': ')[0]] = i.split(': ')[1]
     return d
-
 
 headers = '''Accept: application/json, text/plain, */*
 Accept-Encoding: gzip, deflate, br
@@ -39,10 +89,8 @@ Sec-Fetch-Site: same-origin
 token: pGBJla7R_0NAdtzqQn3V-m9hNweCHsRD8fM3ot0JTHO4AmwVOQoWKLbaQuJvO1Ty96d2JkgbUVt29tZ9K3JMVA
 User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36
 '''
-cookie="_lxsdk_cuid=18645353081c8-08933fc4e72e4a-26021051-384000-18645353081c8; _lxsdk=18645353081c8-08933fc4e72e4a-26021051-384000-18645353081c8; e_b_id_352126=a2b85b6dd5c63b8d8649ebdfa2ac7065; WEBDFPID=0z0vvz2025005x11zyu5940879809zy8813546399v5979585z0wzwy4-1991558412669-1676198412669GASYSOCfd79fef3d01d5e9aadc18ccd4d0c95073864; token=_oYUS_bFdTei6BozV-lwtPfd8hqox6WDdGPXxFlsRiTKbQ1SRqyouZ4pkur9gK-3EcpexMWjdHYngjnCCLpVvw; bizType=2; accountId=137049837; name=HJH1427101411; hospitalName=%25E9%259D%2592%25E5%25B2%259B%25E7%2599%25BE%25E5%25AF%25BF%25E4%25BA%2592%25E8%2581%2594%25E7%25BD%2591%25E5%258C%25BB%25E9%2599%25A2; accountType=2; acctId=10103; logan_session_token=6gd6ayudqos6rxwzt21w; _lxsdk_s=18654457767-15b-e48-e5f%7C%7C9"
-token="_oYUS_bFdTei6BozV-lwtPfd8hqox6WDdGPXxFlsRiTKbQ1SRqyouZ4pkur9gK-3EcpexMWjdHYngjnCCLpVvw"
 headers = trans(headers)
-headers["Cookie"]=cookie
+headers["Cookie"]=cookies
 headers['token']=token
 
 def load_config():
@@ -55,7 +103,7 @@ def load_config():
 def get_info(starttime="",endtime=""):
     d=dict()
     try:
-        r=requests.get(info_url%(starttime,endtime),headers=headers,timeout=1)
+        r=requests.get(info_url%(starttime,endtime),headers=headers,timeout=1,verify = False)
         d=json.loads(r.text)
     except requests.exceptions.ReadTimeout:
         d['success']="error"
@@ -68,7 +116,7 @@ def submit(workShiftIds):
     d=dict()
     try:
         submit_form = {"workShiftIds": workShiftIds, "applyType": 1}
-        r = requests.post(submit_url, headers=headers, json=submit_form)
+        r = requests.post(submit_url, headers=headers, json=submit_form,verify = False)
         d=json.loads(r.text)
     except:
         d['success']="error"
